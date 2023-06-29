01cb254fcd4acdea61af193b24c68aef8ff05fd3
Nazarii Plotnichenko <nazarii.plotnichenko@gmail.com>
2023-06-29 23:21:00 +0300

    Added image and split algorithm

---
+++ src/PuzzleButton.java
@@ -0,0 +1,15 @@
+package src;
+import javax.swing.*;
+import java.awt.*;
+
+public class PuzzleButton {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setVisible(true);
+        JButton jbutton = new JButton("Button");
+        //jbutton.addActionListener(new Color(12, 15,63));
+        jFrame.add(jbutton);
+    }
+
+}
